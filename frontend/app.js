6e3dccb9587a0555b8997be60ba5c77c72d2e12f
tariq126 <148344693+tariq126@users.noreply.github.com>
2025-11-23 22:47:58 +0200

    Update API base URL and initialize app state

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // --- CONFIG ---
-    const API_BASE_URL = 'http://localhost:3001'; // From your server.js
+    const API_BASE_URL = 'http://44.197.208.101:3001'; // From your server.js
 
     // --- STATE ---
     let appState = {
@@ -494,4 +494,5 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     init();
+
 });
