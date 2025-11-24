b0cc1d24e7a6433dbd8704d20bdf524a6ace7f64
Ubuntu <ubuntu@ip-172-31-9-244.ec2.internal>
2025-11-24 10:26:59 +0000

    clean k8s

--- application/frontend/app.js
+++ application/frontend/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // --- CONFIG ---
-    const API_BASE_URL = 'http://44.197.208.101:3001'; // From your server.js
+    const API_BASE_URL = 'http://98.86.180.196:3001'; // From your server.js
 
     // --- STATE ---
     let appState = {
