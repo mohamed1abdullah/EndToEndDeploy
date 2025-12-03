f72e6f16edb54e291292b89d0ee2a3893217aa92
mohamed1abdullah <mohamedelghzaly001@gmail.com>
2025-12-03 21:49:57 +0200

    updated

--- application/frontend/app.js
+++ application/frontend/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // --- CONFIG ---
-    const API_BASE_URL = "http://3.237.81.255:3001";
+    const API_BASE_URL = "http://backend.be-ns.svc.cluster.local:3001";
 
     // --- STATE ---
     let appState = {
