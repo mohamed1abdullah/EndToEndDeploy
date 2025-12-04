56ac92b9c9664cf8b76b1a2701795ba846644ac3
tariq126 <tariqmotawea10@gmail.com>
2025-12-04 20:51:19 +0200

    Secured Dockerfiles (Distroless/Unprivileged) and hardened K8s deployment

--- application/frontend/app.js
+++ application/frontend/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // --- CONFIG ---
-    const API_BASE_URL = "http://3.237.81.255:3001";
+    const API_BASE_URL = "http://13.223.247.185:3001";
 
     // --- STATE ---
     let appState = {
